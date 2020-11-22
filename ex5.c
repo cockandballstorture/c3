2745662582adaa03d06d6f00090c75e9a9ca6703
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2020-11-22 21:38:03 +0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
 int main()
 {
-    int x,b;
+    int x,b,k;
     scanf("%d",&x);
     b=x%10;
+    k=(x/10)%10;
     switch (b) {
     case 1:
-        printf("%d апельсин",x);
-        break;
+          switch(k){
+              case 1:
+                printf("%d апельсинов",x);
+                break;
+              default:
+                printf("%d апельсин",x);
+                break;
+          }
+        break;  
     case 2	:
-        printf("%d апельсина",x);
-         break;
+                  switch(k){
+              case 1:
+                printf("%d апельсинов",x);
+                break;
+              default:
+                printf("%d апельсина",x);
+                break;
+                  }
+        break;
     case 3	:
-        printf("%d апельсина",x);
-         break;
+                     switch(k){
+              case 1:
+                printf("%d апельсинов",x);
+                break;
+              default:
+                printf("%d апельсина",x);
+                break;
+                     }
+        break;
     case 4	:
-        printf("%d апельсина",x);
-         break;
+                             switch(k){
+              case 1:
+                printf("%d апельсинов",x);
+                break;
+              default:
+                printf("%d апельсина",x);
+                break;
+                     }
+        break;
     default:
         printf("%d апельсинов",x);
     }
 }
-  
